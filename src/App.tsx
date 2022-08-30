1a03fdf5633dd333335977fdc02a546f99152f56
Alex <aliakseishulha2021@gmail.com>
2022-08-30 15:55:46 +0300

    create components Loader and ErrorMessage

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,19 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './index.css';
 import {Product} from "./components/Product";
-import axios from "axios";
-import {IProduct} from "./models";
+import {useProducts} from "./hooks/products";
+import {Loader} from "./components/Loader";
+import {ErrorMessage} from "./components/ErrorMessage";
 
 function App() {
 
-	const [products, setProducts] = useState<IProduct[]>([])
-
-	const fetchProducts = async () => {
-		const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
-		setProducts(response.data)
-	}
-
-	useEffect(() => {
-		fetchProducts()
-	}, [])
+	const {products, loading, error} = useProducts()
 
 	return (
 		<div className="container mx-auto max-w-2xl pt-5">
-
-			{products.map(product => <Product product={product} key={product.id}/>)}
-
+			{ loading && <Loader/> }
+			{ error && <ErrorMessage/> }
+			{ products.map(product => <Product product={product} key={product.id}/>) }
 		</div>
 	);
 }
---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const ErrorMessage = () => {
+
+	return (
+		<div>
+			<p className="text-center">Loading...</p>
+		</div>
+	);
+};
+
